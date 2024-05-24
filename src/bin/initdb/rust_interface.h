295bea2d270cf847a25894f4f2d71f5c7ea15d83
NContinanza <nicolas.continanza@lambdaclass.com>
2024-05-23 22:58:02 -0300

    Add rust_interface.h

--- src/bin/initdb/rust_interface.h
+++ src/bin/initdb/rust_interface.h
@@ -1,6 +1,10 @@
 #ifndef RUST_INTERFACE_H
 #define RUST_INTERFACE_H
 
-extern int rust_function(int number);
+typedef struct {
+    char* name;
+} BindingTest;
+
+BindingTest* NewBindingTest(const char* name);
 
 #endif /* RUST_INTERFACE_H */
