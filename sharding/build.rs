c6a76443fddc8ebcf546c5a97819dd21f2e79575
francupp9-12 <fcuppari@fi.uba.ar>
2024-08-07 12:01:44 -0300

    Test commit

--- sharding/build.rs
+++ sharding/build.rs
@@ -1,4 +1,4 @@
 fn main() {
-    println!("cargo:rustc-link-lib=framework=IOKit");
-    println!("cargo:rustc-link-lib=framework=CoreFoundation");
+/*    println!("cargo:rustc-link-lib=framework=IOKit");
+    println!("cargo:rustc-link-lib=framework=CoreFoundation");*/
 }
